e4deb75746ee87336cb47cf410f17f18b80c63c1
Mark Lovett <mark@marklovett.com>
2015-05-17 23:57:04 -0400

    update

--- www/dist/dist_js/app/config.js
+++ www/dist/dist_js/app/config.js
@@ -1,5 +1,6 @@
 angular.module('your_app_name.config', [])
-.constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
+// .constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
+.constant('WORDPRESS_API_URL', 'http://www.marklovettstudio.com/?json=get_recent_posts')
 .constant('GCM_SENDER_ID', '574597432927')
 
 ;
